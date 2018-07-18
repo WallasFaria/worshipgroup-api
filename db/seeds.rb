8ca8197b0aa8e23c22e659c9993a22f97d6a9516
Wallas Faria <wallasfaria@hotmail.com>
2018-07-18 00:25:14 -0300

    Creates instrument seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Instrument.create([
+  { name: 'Baixo', icon: '/icons/bass-guitar.svg' },
+  { name: 'Percussão', icon: '/icons/conga.svg' },
+  { name: 'Bateria', icon: '/icons/drums.svg' },
+  { name: 'Guitarra', icon: '/icons/electric-guitar-1.svg' },
+  { name: 'Violão', icon: '/icons/guitar.svg' },
+  { name: 'Violino', icon: '/icons/violin.svg' },
+  { name: 'Violoncelo', icon: '/icons/violoncello.svg' },
+  { name: 'Piano', icon: '/icons/piano_2.svg' },
+  { name: 'Teclado', icon: '/icons/electric-keyboard.svg' },
+  { name: 'Voz', icon: '/icons/microphone-with-wire.svg' },
+  { name: 'Segunda voz', icon: '/icons/microphone.svg' },
+  { name: 'Trompete', icon: '/icons/trumpet.svg' }
+])
