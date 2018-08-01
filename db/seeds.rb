348956b02e3fe54b5dc1fd66acb3a6ef3f441f0f
Wallas Faria <wallasfaria@hotmail.com>
2018-07-31 21:17:52 -0300

    Renames and adds Role seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Instrument.create([
-  { name: 'Baixo', icon: '/icons/bass-guitar.svg' },
-  { name: 'Percussão', icon: '/icons/conga.svg' },
-  { name: 'Bateria', icon: '/icons/drums.svg' },
-  { name: 'Guitarra', icon: '/icons/electric-guitar-1.svg' },
-  { name: 'Violão', icon: '/icons/guitar.svg' },
-  { name: 'Violino', icon: '/icons/violin.svg' },
-  { name: 'Violoncelo', icon: '/icons/violoncello.svg' },
-  { name: 'Piano', icon: '/icons/piano_2.svg' },
-  { name: 'Teclado', icon: '/icons/electric-keyboard.svg' },
-  { name: 'Voz', icon: '/icons/microphone-with-wire.svg' },
-  { name: 'Segunda voz', icon: '/icons/microphone.svg' },
-  { name: 'Trompete', icon: '/icons/trumpet.svg' }
+Role.create([
+  { name: 'Baixista', icon: '/icons/bass-guitar.svg' },
+  { name: 'Percussionista', icon: '/icons/conga.svg' },
+  { name: 'Baterista', icon: '/icons/drums.svg' },
+  { name: 'Guitarrista', icon: '/icons/electric-guitar-1.svg' },
+  { name: 'Violonista', icon: '/icons/guitar.svg' },
+  { name: 'Violinista', icon: '/icons/violin.svg' },
+  { name: 'Violoncelista', icon: '/icons/violoncello.svg' },
+  { name: 'Pianista', icon: '/icons/piano_2.svg' },
+  { name: 'Tecladista', icon: '/icons/electric-keyboard.svg' },
+  { name: 'Vocalista', icon: '/icons/microphone-with-wire.svg' },
+  { name: 'Backing vocal', icon: '/icons/microphone.svg' },
+  { name: 'Trompetista', icon: '/icons/trumpet.svg' }
+  { name: 'Acordeonista', icon: '/icons/accordion.svg' }
+  { name: 'Clarinetista', icon: '/icons/inclined-clarinet.svg' }
+  { name: 'Flautista', icon: '/icons/flute.svg' }
+  { name: 'Harpista', icon: '/icons/harp.svg' }
+  { name: 'Saxofonista', icon: '/icons/saxophon.svg' }
+  { name: 'Cavaquinista', icon: '/icons/ukelele-2.svg' }
 ])
