12d330c1bb19ac71ae5c19874cd22eda79471962
Wallas Faria <wallasfaria@hotmail.com>
2018-08-15 00:57:24 -0300

    Sets SECRET_KEY_BASE env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,3 @@
 Devise.setup do |config|
-  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
+  config.secret_key = MyApp::Application.config.secret_key_base || ENV['SECRET_KEY_BASE']
 end
